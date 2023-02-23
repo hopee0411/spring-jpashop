43873d251d8431579c5bbd7439a215cfb914e565
hopee0411 <hopee0411@naver.com>
2023-02-23 20:00:04 +0900

    h2 연결, live template tdd 설정

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+
+}
