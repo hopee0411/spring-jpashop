31c74a6a3b0a4b46cccc59d9b386927352e50671
hopee0411 <hopee0411@naver.com>
2023-02-23 13:59:15 +0900

    Hello.class Test

---
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter @Getter @ToString
+public class Hello {
+    private String data;
+
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		System.out.println("hello = " + hello);
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
