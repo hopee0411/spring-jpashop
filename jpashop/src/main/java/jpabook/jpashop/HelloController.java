d719987ff8b651c91074c95ef4eab4d7ea13b735
hopee0411 <hopee0411@naver.com>
2023-02-24 18:37:28 +0900

    create MemberRepository

--- jpashop/src/main/java/jpabook/jpashop/HelloController.java
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HelloController {
 
@@ -11,5 +13,7 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
+
     }
+
 }
--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,14 +7,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
-    @Test
-    @DisplayName("test")
-    void test() {
-        // given
-        
-        // when
-        
-        // then
-    }
-
 }
