cab00469cf61bfb6efa4460c7cdb173c2cf4f3bb
hopee0411 <hopee0411@naver.com>
2023-04-03 18:42:49 +0900

    230403

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
+}
